4db3036d375348f8087b8a94579b44f59b0f07a8
Empyrz <35655138+empyrz@users.noreply.github.com>
2024-03-28 22:34:02 +0100

    Update request.go

--- request.go
+++ request.go
@@ -4,16 +4,18 @@ import (
 	"bytes"
 	"fmt"
 	"log"
+	"log/syslog"
 	"net"
 	"strconv"
 	"time"
 )
 
+// Represents a TFTP request.
 type Request struct {
 	Filename    string        // The requested filename.
 	Mode        string        // The request mode. Can usually be ignored.
 	addr        *net.UDPAddr // Peer address
-	ackChannel  chan uint16  // Channel which receives acknowledgement numbers.
+	ackChannel  chan uint16   // Channel which receives acknowledgement numbers.
 	server      *Server
 	blockSize   uint16
 	blockBuf    bytes.Buffer
@@ -32,41 +34,122 @@ func errorLog(format string, args ...interface{}) {
 	log.Printf("[ERROR] "+format, args...)
 }
 
+func (req *Request) sendAndWaitForAck(txFunc func() error, blockNum uint16) error {
+	t := time.Now()
+
+loop:
+	for {
+		err := txFunc()
+		if err != nil {
+			return err
+		}
+
+		select {
+		case brctl := <-req.ackChannel:
+			if brctl == blockNum {
+				break loop
+			}
+		case <-time.After(req.server.RetransmissionTimeout):
+			if time.Now().After(t.Add(req.server.RequestTimeout)) {
+				req.terminate()
+				return ErrTimedOut
+			}
+		}
+	}
+
+	return nil
+}
+
+func (req *Request) fbSendData() error {
+	return req.server.sendTftpDataPacket(req.addr, req.blockNum, req.blockBuf.Bytes())
+}
+
+func (req *Request) flushBlock(isFinal bool) error {
+	if req.blockBuf.Len() == 0 && !isFinal {
+		return nil
+	}
+
+	if !isFinal && req.blockBuf.Len() != int(req.blockSize) {
+		errorLog("Unexpected short block")
+		return fmt.Errorf("unexpected short block")
+	}
+
+	err := req.sendAndWaitForAck(req.fbSendData, req.blockNum)
+
+	if err != nil {
+		return err
+	}
+
+	req.blockBuf.Truncate(0)
+	req.blockNum++
+	return nil
+}
+
+func (req *Request) setOption(k, v string) error {
+	switch k {
+	case "blksize":
+		n, err := strconv.ParseUint(v, 10, 16)
+		if err != nil {
+			return err
+		}
+		debugLog("Block size called: %d", n)
+		return req.setBlockSize(uint16(n))
+
+	default:
+		// not supported
+		return fmt.Errorf("not supported")
+	}
+}
+
 func (req *Request) setBlockSize(blockSize uint16) error {
-	debugLog("Setting block size to: %d", blockSize)
+	// Output the value of blockSize before assignment
+	syslogger, err := syslog.New(syslog.LOG_INFO, "syslog_example")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("Block size before assignment:", blockSize)
 
 	if blockSize < 8 || blockSize > 65464 {
 		return fmt.Errorf("invalid block size: %d not in 8 <= blockSize <= 65464", blockSize)
 	}
 
+	// Initialize options map if it's nil
+	if req.options == nil {
+		req.options = make(map[string]string)
+	}
+
+	// Assign the value to blockSize
 	req.blockSize = blockSize
 
-	debugLog("Block size set successfully: %d", req.blockSize)
-
+	// Output the value of blockSize after assignment
+	log.Println("Block size after assignment:", req.blockSize)
+	// Assign the value to req.options["blksize"]
+	log.Println("av conv blksize", req.options["blksize"])
+	req.options["blksize"] = strconv.FormatUint(uint64(blockSize), 10)
+	log.Println("ap conv blksize", req.options["blksize"])
 	return nil
 }
 
-func (req *Request) Write(data []byte) (int, error) {
-	debugLog("Received write request. Data length: %d", len(data))
-
-	n := 0
-	for len(data) > 0 {
-		nn, err := req.write(data)
-		n += nn
-		if err != nil {
-			errorLog("Error writing data: %v", err)
-			return n, err
-		}
-		data = data[nn:]
-	}
-	return n, nil
+// Returns the address of the client which initiated the TFTP transfer.
+func (req *Request) ClientAddress() net.UDPAddr {
+	return *req.addr
 }
 
-func (req *Request) write(data []byte) (int, error) {
-	debugLog("Writing data block. Data length: %d", len(data))
+// Request already closed.
+var ErrClosed = fmt.Errorf("Request already closed")
 
+// Request timed out.
+var ErrTimedOut = fmt.Errorf("Request timed out")
+
+// Writes bytes which represent data from the file requested. The data provided
+// is logically appended to the data provided in previous calls to Write. There
+// are no particular requirements on the size of the buffer passed, and the
+// size passed may vary between calls to Write.
+//
+// The amount of data written is returned. Note that not all data provided may
+// be written. Returns ErrClosed if the request has already been terminated.
+func (req *Request) write(data []byte) (int, error) {
 	if req.terminated {
-		errorLog("Request already terminated")
 		return 0, ErrClosed
 	}
 
@@ -82,91 +165,97 @@ func (req *Request) write(data []byte) (int, error) {
 	}
 
 	if req.blockBuf.Len() < int(req.blockSize) {
-		debugLog("Data written successfully. Length: %d", L)
 		return L, nil
 	}
 
 	err := req.flushBlock(false)
 	if err != nil {
-		errorLog("Error flushing block: %v", err)
 		return 0, err
 	}
-
-	debugLog("Data written successfully. Length: %d", L)
+	log.Println("data", L)
 	return L, nil
 }
 
-func (req *Request) flushBlock(isFinal bool) error {
-	debugLog("Flushing data block. Is final: %t", isFinal)
+// Writes bytes which represent data from the file requested. The data provided
+// is logically appended to the data provided in previous calls to Write. There
+// are no particular requirements on the size of the buffer passed, and the
+// size passed may vary between calls to Write. In other words, this method
+// implements the semantics of io.Writer.
+//
+// Returns ErrClosed if the request has already been terminated.
+func (req *Request) Write(data []byte) (int, error) {
 
-	if req.blockBuf.Len() == 0 && !isFinal {
-		debugLog("Empty block buffer. Nothing to flush.")
-		return nil
-	}
-
-	if !isFinal && req.blockBuf.Len() != int(req.blockSize) {
-		errorLog("Unexpected short block")
-		return fmt.Errorf("unexpected short block")
-	}
-
-	err := req.sendAndWaitForAck(req.fbSendData, req.blockNum)
-	if err != nil {
-		errorLog("Error sending data block: %v", err)
-		return err
-	}
-
-	req.blockBuf.Truncate(0)
-	req.blockNum++
-
-	debugLog("Data block flushed successfully")
-	return nil
-}
-
-func (req *Request) fbSendData() error {
-	debugLog("Sending data block. Block number: %d", req.blockNum)
-
-	return req.server.sendTftpDataPacket(req.addr, req.blockNum, req.blockBuf.Bytes())
-}
-
-func (req *Request) sendAndWaitForAck(txFunc func() error, blockNum uint16) error {
-	debugLog("Sending data and waiting for acknowledgment. Block number: %d", blockNum)
-
-	t := time.Now()
-
-loop:
-	for {
-		err := txFunc()
+	n := 0
+	for len(data) > 0 {
+		nn, err := req.write(data)
+		n += nn
 		if err != nil {
-			errorLog("Error sending data: %v", err)
-			return err
+			return n, err
 		}
-
-		select {
-		case brctl := <-req.ackChannel:
-			if brctl == blockNum {
-				debugLog("Acknowledgment received for block number: %d", blockNum)
-				break loop
-			}
-		case <-time.After(req.server.RetransmissionTimeout):
-			if time.Now().After(t.Add(req.server.RequestTimeout)) {
-				req.terminate()
-				errorLog("Request timed out")
-				return ErrTimedOut
-			}
-		}
+		data = data[nn:]
 	}
-
-	debugLog("Data sent and acknowledgment received successfully for block number: %d", blockNum)
-	return nil
+	return n, nil
 }
 
 func (req *Request) terminate() {
-	debugLog("Terminating request")
-
 	delete(req.server.requests, req.name())
 	req.terminated = true
 }
 
+// This method must be called when the TFTP transfer has completed
+// successfully. The request enters a terminated state in which further
+// operations are not possible. Multiple calls to this method are
+// inconsequential.
+func (req *Request) Close() {
+	if req.terminated {
+		return
+	}
+	req.flushBlock(true)
+	req.terminate()
+}
+
+// Terminates the TFTP transfer with an error code. The request enters a
+// terminated state in which further operations are not possible. Further calls
+// to this method are inconsequential.
+func (req *Request) WriteError(errNum Error, msg string) {
+	if req.terminated {
+		return
+	}
+	req.server.sendTftpErrorPacket(req.addr, errNum, msg)
+	req.terminate()
+}
+
 func (req *Request) name() string {
+	// TODO: Currently we identify requests by a string formed from the address.
+	// ...
 	return nameFromAddr(req.addr)
+}
+
+// This is run in a per-request goroutine. Blocking is OK.
+func (req *Request) loop() {
+	// TODO: TFTP Option Negotiation support.
+	// At this point we could perform option negotiation by sending OACK etc.
+
+	err := req.finishOptionNegotiation()
+	if err != nil {
+		return
+	}
+
+	// Ready for write
+	req.server.ReadHandler(req)
+}
+
+func (req *Request) finishOptionNegotiation() error {
+	if len(req.options) == 0 {
+		return nil
+	}
+
+	err := req.sendAndWaitForAck(func() error {
+		return req.server.sendTftpOptNegPacket(req.addr, req.options)
+	}, 0)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
